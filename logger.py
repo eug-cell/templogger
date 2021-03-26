53e2752c8bee5957c3b92989a30d20bcea15ef64
Sergej Herbert <herbert.sergej@gmail.com>
2021-03-26 20:41:42 +0100

    refactor: Cleanup and formatting

--- logger.py
+++ logger.py
@@ -2,39 +2,44 @@ from pathlib import Path
 import sqlite3
 import time
 
-sensor_mapping={
-	'28-3c01d6074879':'Speicher_Oben',
-	'28-3c01d607152b':'Speicher_Mitte',
-	'28-3c01d607cdd0':'Solar_VL',
-	'28-3c01d6076837':'Solar_RL',
-	'28-3c01d607b233':'Fußbodenheizung_VL_vM',
-	'28-3c01d60753fe':'Fußbodenheizung_VL_nM',
-	'28-3c01d607666e':'Fußbodenheizung_RL_vM',
-	'28-3c01d6075bd4':'Fußbodenheizung_RL_nM',
-	'28-3c01d6078f50':'Warmwasser_VL',
-	'28-3c01d6070050':'Raumtemperatur',
+sensor_mapping = {
+    "28-3c01d6074879": "Speicher_Oben",
+    "28-3c01d607152b": "Speicher_Mitte",
+    "28-3c01d607cdd0": "Solar_VL",
+    "28-3c01d6076837": "Solar_RL",
+    "28-3c01d607b233": "Fußbodenheizung_VL_vM",
+    "28-3c01d60753fe": "Fußbodenheizung_VL_nM",
+    "28-3c01d607666e": "Fußbodenheizung_RL_vM",
+    "28-3c01d6075bd4": "Fußbodenheizung_RL_nM",
+    "28-3c01d6078f50": "Warmwasser_VL",
+    "28-3c01d6070050": "Raumtemperatur",
 }
 
+
 def read_temp(device_id):
-	sensor_output=Path(f'/sys/bus/w1/devices/{device_id}/w1_slave').read_text()
-	sensor_temperature=int(sensor_output.split('t=')[1])/1000
-	return sensor_temperature
+    sensor_output = Path(f"/sys/bus/w1/devices/{device_id}/w1_slave").read_text()
+    sensor_temperature = int(sensor_output.split("t=")[1]) / 1000
+    return sensor_temperature
+
 
 def save_to_db(cursor, device_id, device_name, temperature):
-	print(device_id, device_name, temperature)
-	cursor.execute(f'''
-	INSERT INTO readings(device_id, device_name, reading_time, reading_value)
-	VALUES ('{device_id}','{device_name}',{int(time.time())},{temperature})
-	''')
+    print(device_id, device_name, temperature)
+    cursor.execute(
+        f"""
+        INSERT INTO readings(device_id, device_name, reading_time, reading_value)
+        VALUES ('{device_id}','{device_name}',{int(time.time())},{temperature})
+        """
+    )
+
 
 def main():
-	conn = sqlite3.connect('db_temperature.db')
-	cursor=conn.cursor()
-	for device_id, device_name in sensor_mapping.items():
-		save_to_db(cursor, device_id, device_name, read_temp(device_id))
-	conn.commit()
-	cursor.close()
-	conn.close()
+    conn = sqlite3.connect("db_temperature.db")
+    cursor = conn.cursor()
+    for device_id, device_name in sensor_mapping.items():
+        save_to_db(cursor, device_id, device_name, read_temp(device_id))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
 
 main()
-
--- setup_db.py
+++ setup_db.py
@@ -1,17 +1,17 @@
 import sqlite3
 
-conn = sqlite3.connect('db_temperature.db')
-cursor=conn.cursor()
-cursor.execute('''
-
-CREATE TABLE IF NOT EXISTS readings (
-	device_id text NOT NULL,
-	device_name text NOT NULL,
-	reading_time int NOT NULL,
-	reading_value real NOT NULL
-);
-
-''')
+conn = sqlite3.connect("db_temperature.db")
+cursor = conn.cursor()
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS readings (
+        device_id text NOT NULL,
+        device_name text NOT NULL,
+        reading_time int NOT NULL,
+        reading_value real NOT NULL
+    );
+    """
+)
 
 cursor.close()
 conn.close()
